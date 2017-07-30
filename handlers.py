3abc67b73e0be6d969e53acd0b984ee48c1d221f
Harshil Goel <harshil158@gmail.com>
2017-07-31 03:47:42 +0530

    gropuds added in image handler

--- handlers.py
+++ handlers.py
@@ -12,11 +12,16 @@ class ImageHandler:
         self.utils = ImageUtils()
 
         self.colors = []
-        self.foregroundColor = -1
+        self.backgroundColor = -1
         self.noises = []
 
+        self.foregroundSize = 0
+        self.backgroundSize = 0
+
+        self.groups = []
+
     def set_up(self):
-        self.im =   self.utils.read_image(self.image_name)
+        self.im = self.utils.read_image(self.image_name)
         self.filtered_image = self.utils.image_filter(self.im)
 
     def find_components(self):
@@ -27,18 +32,25 @@ class ImageHandler:
             im_color = (self.filtered_image == color)
             count = sum(sum(im_color))
 
-            maxCount = -1
+            if count > 0.0005 * self.im.size:
+                if count > self.backgroundSize:
+                    if self.backgroundColor != -1:
+                        self.colors.append(self.backgroundColor)
+                        self.foregroundSize += self.backgroundSize
 
-            if count > 0.05 * self.im.size:
-                if count > maxCount:
-                    if self.foregroundColor != -1:
-                        self.colors.append(self.foregroundColor)
-
-                    maxCount = count
-                    self.foregroundColor = color
+                    self.backgroundSize  = count
+                    self.backgroundColor = color
                     continue
 
+                self.foregroundSize += count
                 self.colors.append(color)
             else:
                 self.noises.append(color)
 
+    def create_groups():
+        groups = [self.backgroundColor, 'background']
+        [groups.append(i, 'foreground') for i in self.colors]
+        [groups.append(i, 'noise') for i in self.noises]
+
+        for group in groups:
+            #TODO make group
